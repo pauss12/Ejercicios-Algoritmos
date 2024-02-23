1e029fcdb2300fc0ad97917acd5e80c38130d5a7
pauss12 <paulamendezsv@gmail.com>
2024-02-23 08:54:23 +0100

    Las dos actividades subidas al DomJudge

---
+++ Actividad_3/recursividadCombinaciones.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+
+int calcularFactorial(int number)
+{
+    int valor;
+
+    valor = 1;
+    if (number <= 1)
+        return 1;
+    else
+    {
+        while (number > 0)
+        {
+            valor = valor * number;
+            number--;
+        }
+    }
+    return (valor);
+}
+
+int calcularCombinatoria(int r, int n)
+{
+    int final;
+    int factorialN;
+    int factorialR;
+    int factorialResta;
+
+    if (r == 0 || r == n)
+        return 1;
+    factorialN = calcularFactorial(n);
+    factorialR = calcularFactorial(r);
+    factorialResta = calcularFactorial(n - r);
+    final = (factorialN)/((factorialR)*(factorialResta));
+
+    return final;
+}
+
+int main(void)
+{
+    int n;
+    int r;
+    int sol;
+    bool continuar = true;
+
+    while (continuar){
+        std::cin >> n >> r;
+        if (n < 0)
+        {
+            continuar = false;
+        }
+        else if (r>n)
+        {
+            std::cout << "ERROR" << std::endl;
+        }
+        else
+        {
+            sol = calcularCombinatoria(r, n);
+            std::cout << sol << std::endl;
+        }
+
+    }; 
+    return 0;
+}
