485505f55dad78e22798a5f66900ad008c0afe8c
pauss12 <paulamendezsv@gmail.com>
2024-03-16 13:03:46 +0100

    le he puesto comentarios al ejercicio de "devolver cambio"

--- Actividad_5/devolverCambio.cpp
+++ Actividad_5/devolverCambio.cpp
@@ -3,6 +3,19 @@
 
 using namespace std;
 
+/*
+    DESCRIPCION:
+
+    El programa resuelve el problema de devolver cambio. El programa recibe una cantidad de dinero de pesetas por terminal y tiene que devolver el cambio en billetes y monedas de 500, 200, 100, 50, 25, 10, 5 y 1; en el menor numero de billetes y monedas posibles.
+
+    COMPLEJIDAD TEMPORAL: La complejidad temporal del programa es de O(1) ya que el programa recorre un array de 8 elementos.
+
+    COMPLEJIDAD ESPACIAL: La complejidad espacial del programa es de O(1) ya que no tiene que reservar memoria dinamicamente.
+
+
+*/
+
+
 void calcularCambio(int *soluciones, int *cambios, int cantidadDevolver)
 {
     int temporal = cantidadDevolver;
