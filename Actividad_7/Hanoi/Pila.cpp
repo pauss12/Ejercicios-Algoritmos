2c50fc36cdaacb3ed6495b6e4d31aaafd9abb330
pauss12 <paulamendezsv@gmail.com>
2024-04-08 19:39:18 +0200

    tenemos la base de la entrega 7 de algoritmos

---
+++ Actividad_7/Hanoi/Pila.cpp
@@ -0,0 +1,30 @@
+#include"Pila.h"
+
+
+Pila::Pila(string name)
+{
+
+}
+
+std::string Pila::nombrePila()
+{
+
+}
+
+void Pila::apilar(int num)
+{
+  cout<<"Apilando disco "<<num<<" en poste "<<name<<endl;
+}
+  
+int Pila::desapilar()
+{    
+
+  cout<<"Desapilando disco "<<num<<" del poste "<<name<<endl;
+
+}
+
+bool Pila::estaVacia()
+{
+
+}
+
